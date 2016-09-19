ed54190e1328a3282bf970685f9e6b5d35de44f2
saulflores95 <saulpolo95@gmail.com>
2016-09-19 14:34:52 -0700

    test

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 
-if (app.get('env') === 'production') {
+if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
