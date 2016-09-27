357cb1d5e160f0fc4bd02cf3c5f50c934b213356
saulflores95 <saulpolo95@gmail.com>
2016-09-26 17:38:48 -0700

    Heroku Routing setup

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var app = express();
 var mongoose = require('mongoose');
 var passport = require('passport');
 
-var herokudb = 'mongodb://<pekub:hypogrifo#2016@ds041556.mlab.com:41556/heroku_cqm1l378';
+var herokudb = 'mongodb://pekub:hypogrifo#2016@ds041556.mlab.com:41556/heroku_cqm1l378';
 
 // connect MongoDB
 mongoose.connect(herokudb, function(err,db){
